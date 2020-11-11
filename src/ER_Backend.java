5ffe33bea77fae620f928fe098b9a33a28dbcf7a
Justin Seitz <justinseitz2016@gmail.com>
2020-11-11 15:27:06 -0800

    made ER_Backend class

--- src/ER_Backend.java
+++ src/ER_Backend.java
@@ -3,64 +3,19 @@
 //STEP 1. Import required packages
 import java.sql.*;
 
-public class UserInstance {
+public class ER_Backend {
    // JDBC driver name and database URL
    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
    static final String DB_URL = "jdbc:mysql://localhost/cs532"; //change depending on database
 
    //  Database credentials
-   //static final String USER = "root";
-   //static final String PASS = "root";
-   static String USER;
-   static String PASS;
+   static final String USER = "root";
+   static final String PASS = "root";
+
+   public static void main (String[] args) {
+	   showPatients();
+   }
    
-   public UserInstance() {
-	   USER = "root";
-	   PASS = "root";
-   }
-   public UserInstance(String username, String password) {
-	   USER = username;
-	   PASS = password;
-   }
-
-   public void changeLogin(String username, String password) {
-	   USER = username;
-	   PASS = password;
-   }
-
-   public boolean checkLogin() {
-	   Connection conn = null;
-	   try{
-	//STEP 2: Register JDBC driver
-	      Class.forName("com.mysql.jdbc.Driver");
-
-	      //STEP 3: Open a connection
-	      System.out.println("Connecting to database...");
-	      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-	      //STEP 6: Clean-up environment
-	      conn.close();
-	      return true;
-	   }catch(SQLException se){
-	      //Handle errors for JDBC
-	      //se.printStackTrace();
-	      return false;
-	   }catch(Exception e){
-	      //Handle errors for Class.forName
-	      //e.printStackTrace();
-	      return false;
-	   }finally{
-	      //finally block used to close resources
-	      try{
-	         if(conn!=null)
-	            conn.close();
-	      }catch(SQLException se){
-	         //se.printStackTrace();
-	      }//end finally try
-	   }//end try
-	   //System.out.println("Goodbye!"); 
-   }
-
    public static void showPatients() {
    Connection conn = null;
    Statement stmt = null;
