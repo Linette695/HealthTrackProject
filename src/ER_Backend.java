b4362e8c55d61be49599c0df6de8d16b100a76e1
Justin Seitz <justinseitz2016@gmail.com>
2020-11-11 17:39:26 -0800

    Patient returnPatient(int id) now works

--- src/ER_Backend.java
+++ src/ER_Backend.java
@@ -15,14 +15,75 @@ public class ER_Backend {
 
 	//Using main only for isolated testing, comment out when using with UI
 	public static void main (String[] args) {
+		
 		List<Patient> patientList = returnPatientInfoAll();
 		for (int i=0; i<patientList.size(); i++) {
 			patientList.get(i).printInfo();
 		}
+		
+		Patient p = returnPatient(10);
+		p.printInfo();
 	}
 
 	public ER_Backend() {
 
+	}
+
+	public static Patient returnPatient(int id) {
+		Connection conn = null;
+		Statement stmt = null;
+		Patient patient = new Patient();
+		try{
+			//STEP 2: Register JDBC driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//STEP 3: Open a connection
+			System.out.println("Connecting to a selected database...");
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			System.out.println("Connected database successfully...");
+
+			//STEP 4: Execute a query
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+
+			String sql = "SELECT * FROM Patients WHERE pid="+id;
+			ResultSet rs = stmt.executeQuery(sql);
+			//STEP 5: Extract data from result set
+			while(rs.next()){
+				//Retrieve by column name
+				patient.pid = rs.getInt("pid");
+				patient.pfirstname = rs.getString("pfirstname");
+				patient.plastname = rs.getString("plastname");
+				patient.tnumber = rs.getInt("tnumber");
+				patient.address = rs.getString("address");
+				patient.iid = rs.getInt("iid");
+				patient.dob = rs.getDate("dob");
+				patient.gender = rs.getString("gender");
+				patient.pcp = rs.getInt("pcp");
+			}
+			rs.close();
+		}catch(SQLException se){
+			//Handle errors for JDBC
+			se.printStackTrace();
+		}catch(Exception e){
+			//Handle errors for Class.forName
+			e.printStackTrace();
+		}finally{
+			//finally block used to close resources
+			try{
+				if(stmt!=null)
+					conn.close();
+			}catch(SQLException se){
+			}// do nothing
+			try{
+				if(conn!=null)
+					conn.close();
+			}catch(SQLException se){
+				se.printStackTrace();
+			}//end finally try
+		}//end try
+		System.out.println("Goodbye!");
+		return patient;
 	}
 
 	public static List<Patient> returnPatientInfoAll() {
@@ -57,7 +118,7 @@ public class ER_Backend {
 				patient.dob = rs.getDate("dob");
 				patient.gender = rs.getString("gender");
 				patient.pcp = rs.getInt("pcp");
-				
+
 				patientList.add(patient);
 			}
 			rs.close();
