a07a93b781335f4c566de498e07deb7cb482c49f
Linette695 <Linette695@users.noreply.github.com>
2020-11-23 16:33:48 -0800

    Updated SCHED(Added weekly schedule report window)

--- src/SCHED_WeeklyReport.java
+++ src/SCHED_WeeklyReport.java
@@ -1,14 +1,30 @@
 import java.awt.EventQueue;
+import java.util.Properties;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+import org.jdatepicker.impl.JDatePanelImpl;
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
+
 import javax.swing.JComboBox;
+import javax.swing.JFormattedTextField.AbstractFormatter;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SpringLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
+import javax.swing.JFormattedTextField;
 
 public class SCHED_WeeklyReport {
 
 	private JFrame frmGeneratingWeeklyReport;
+	private SpringLayout springLayout;
 
 	/**
 	 * Launch the application.
@@ -56,6 +72,22 @@ public class SCHED_WeeklyReport {
 		JButton btnGenerateReport = new JButton("Generate Report");
 		btnGenerateReport.setBounds(290, 9, 136, 21);
 		frmGeneratingWeeklyReport.getContentPane().add(btnGenerateReport);
+		
+		UtilDateModel model = new UtilDateModel();
+		Properties props = new Properties();
+		props.put("text.today", "Today");
+		props.put("text.month", "Month");
+		props.put("text.year", "Year");
+		JDatePanelImpl datePanel = new JDatePanelImpl(model,props);
+		
+		//TRYING TO USE JDatePicker
+//		JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, null);
+//		datePicker.setBounds(262, 40, 164, 58);
+//		
+//		frmGeneratingWeeklyReport.getContentPane().add(datePicker);
+//		datePicker.add(datePicker.getJFormattedTextField());
+//		
+//		System.out.println(datePicker.getJFormattedTextField().getText());
 	}
 
 }
