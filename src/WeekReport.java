ea07cd39a9a0108ff73cda3ee338f2c5964b1838
allisonschroder <71051639+allisonschroder@users.noreply.github.com>
2020-12-06 15:13:42 -0800

    Update WeekReport.java

--- src/WeekReport.java
+++ src/WeekReport.java
@@ -63,7 +63,7 @@ public class WeekReport {
 		Statement stmt = null;
 		try{
 			//STEP 2: Register JDBC driver
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName(JDBC_DRIVER);
 
 			//STEP 3: Open a connection
 			System.out.println("Connecting to database...");
