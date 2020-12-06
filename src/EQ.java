e86179e412094096cd1eb2fc98fb9443bd2b8669
allisonschroder <71051639+allisonschroder@users.noreply.github.com>
2020-12-06 13:34:11 -0800

    Update EQ.java

--- src/EQ.java
+++ src/EQ.java
@@ -18,7 +18,7 @@ import java.awt.event.ActionEvent;
 
 public class EQ {
 
-	private JFrame frmEquiptmentInventory;
+	private JFrame frmEquipmentInventory;
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
@@ -35,7 +35,7 @@ public class EQ {
 			public void run() {
 				try {
 					EQ window = new EQ();
-					window.frmEquiptmentInventory.setVisible(true);
+					window.frmEquipmentInventory.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -64,7 +64,7 @@ public class EQ {
 
 	//Method invoked when user wants to report a new equipment problem
 	public void EQNewProblem() {
-		frmEquiptmentInventory.setVisible(true);					
+		frmEquipmentInventory.setVisible(true);					
 		
 		tabbedPane.setEnabledAt(0, true);			//Make sure the Report Equipment problem tab is enabled
 		tabbedPane.setSelectedIndex(0);				//Make sure the Report Equipment problem tab is the one being displayed
@@ -77,7 +77,7 @@ public class EQ {
 	
 	//Method invoked when user wants to update an equipment problem
 	public void EQUpdateProblem() {
-		frmEquiptmentInventory.setVisible(true);					
+		frmEquipmentInventory.setVisible(true);					
 		
 		tabbedPane.setEnabledAt(1, true);			//Make sure the update an equipment problem tab is enabled
 		tabbedPane.setSelectedIndex(1);				//Make sure the update an equipment problem tab is the one being displayed
@@ -90,7 +90,7 @@ public class EQ {
 	
 	//Method invoked when user wants to query the equipment database
 		public void EQQueryEquipment() {
-			frmEquiptmentInventory.setVisible(true);					
+			frmEquipmentInventory.setVisible(true);					
 			
 			tabbedPane.setEnabledAt(2, true);			//Make sure the query equipment tab is enabled
 			tabbedPane.setSelectedIndex(2);				//Make sure the query equipment tab is the one being displayed
@@ -103,7 +103,7 @@ public class EQ {
 
 		//Method invoked when user wants to access the vendor list maintenance functionalities 
 		public void EQVendorListMaintenance() {
-			frmEquiptmentInventory.setVisible(true);					
+			frmEquipmentInventory.setVisible(true);					
 			
 			tabbedPane.setEnabledAt(3, true);			//Make sure the vendor list maintenance tab is enabled
 			tabbedPane.setSelectedIndex(3);				//Make sure the vendor list maintenance tab is the one being displayed
@@ -116,7 +116,7 @@ public class EQ {
 
 		//Method invoked when user wants to generate a new report
 		public void EQGenerateNewReport() {
-			frmEquiptmentInventory.setVisible(true);					
+			frmEquipmentInventory.setVisible(true);					
 			
 			tabbedPane.setEnabledAt(4, true);			//Make sure the generate new report tab is enabled
 			tabbedPane.setSelectedIndex(4);				//Make sure the generate new report tab is the one being displayed
@@ -132,136 +132,136 @@ public class EQ {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmEquiptmentInventory = new JFrame();
-		frmEquiptmentInventory.setTitle("Equiptment Inventory & Maintenance ");
-		frmEquiptmentInventory.setBounds(100, 100, 481, 329);
-		frmEquiptmentInventory.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frmEquiptmentInventory.getContentPane().setLayout(null);
+		frmEquipmentInventory = new JFrame();
+		frmEquipmentInventory.setTitle("Equipment Inventory & Maintenance ");
+		frmEquipmentInventory.setBounds(100, 100, 481, 329);
+		frmEquipmentInventory.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frmEquipmentInventory.getContentPane().setLayout(null);
 		
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(0, 0, 467, 292);
-		frmEquiptmentInventory.getContentPane().add(tabbedPane);
+		frmEquipmentInventory.getContentPane().add(tabbedPane);
 		
 		VendorListSubsystem = new EQ_QueryVendorInfo();
 		
-		JPanel ReportEquiptmentProblemPanel = new JPanel();
-		tabbedPane.addTab("Report An Equiptment Problem", null, ReportEquiptmentProblemPanel, null);
-		ReportEquiptmentProblemPanel.setLayout(null);
+		JPanel ReportEquipmentProblemPanel = new JPanel();
+		tabbedPane.addTab("Report An Equipment Problem", null, ReportEquipmentProblemPanel, null);
+		ReportEquipmentProblemPanel.setLayout(null);
 		
-		JLabel lblEquiptmentId = new JLabel("Equiptment ID:");
-		lblEquiptmentId.setBounds(10, 13, 107, 13);
-		ReportEquiptmentProblemPanel.add(lblEquiptmentId);
+		JLabel lblEquipmentId = new JLabel("Equipment ID:");
+		lblEquipmentId.setBounds(10, 13, 107, 13);
+		ReportEquipmentProblemPanel.add(lblEquipmentId);
 		
 		textField = new JTextField();
 		textField.setText("Enter ID #");
 		textField.setColumns(10);
 		textField.setBounds(120, 10, 96, 19);
-		ReportEquiptmentProblemPanel.add(textField);
+		ReportEquipmentProblemPanel.add(textField);
 		
 		JLabel lblTypeOfProblem = new JLabel("Type of problem:");
 		lblTypeOfProblem.setBounds(10, 36, 125, 13);
-		ReportEquiptmentProblemPanel.add(lblTypeOfProblem);
+		ReportEquipmentProblemPanel.add(lblTypeOfProblem);
 		
 		JComboBox comboBox_5 = new JComboBox();
 		comboBox_5.setModel(new DefaultComboBoxModel(new String[] {"Problem type #1", "Problem type #2", "...", "Other"}));
 		comboBox_5.setBounds(120, 36, 136, 19);
-		ReportEquiptmentProblemPanel.add(comboBox_5);
+		ReportEquipmentProblemPanel.add(comboBox_5);
 		
 		JLabel lblDescribeTheProblem = new JLabel("Describe the problem:");
 		lblDescribeTheProblem.setBounds(10, 59, 158, 13);
-		ReportEquiptmentProblemPanel.add(lblDescribeTheProblem);
+		ReportEquipmentProblemPanel.add(lblDescribeTheProblem);
 		
 		JTextArea txtrEnterDescription = new JTextArea();
 		txtrEnterDescription.setText("Enter Description");
 		txtrEnterDescription.setBounds(120, 76, 311, 71);
-		ReportEquiptmentProblemPanel.add(txtrEnterDescription);
+		ReportEquipmentProblemPanel.add(txtrEnterDescription);
 		
 		JButton btnReportProblem = new JButton("Report Problem");
 		btnReportProblem.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnReportProblem.setBounds(140, 163, 181, 21);
-		ReportEquiptmentProblemPanel.add(btnReportProblem);
+		ReportEquipmentProblemPanel.add(btnReportProblem);
 		
-		JPanel UpdateAnEquiptmentProblemPanel = new JPanel();
-		tabbedPane.addTab("Updating An Equiptment Problem", null, UpdateAnEquiptmentProblemPanel, null);
-		UpdateAnEquiptmentProblemPanel.setLayout(null);
+		JPanel UpdateAnEquipmentProblemPanel = new JPanel();
+		tabbedPane.addTab("Updating An Equipment Problem", null, UpdateAnEquipmentProblemPanel, null);
+		UpdateAnEquipmentProblemPanel.setLayout(null);
 		
-		JLabel lblEquiptmentProblemId = new JLabel("Equiptment problem ID:");
-		lblEquiptmentProblemId.setBounds(10, 13, 154, 13);
-		UpdateAnEquiptmentProblemPanel.add(lblEquiptmentProblemId);
+		JLabel lblEquipmentProblemId = new JLabel("Equiptment problem ID:");
+		lblEquipmentProblemId.setBounds(10, 13, 154, 13);
+		UpdateAnEquipmentProblemPanel.add(lblEquipmentProblemId);
 		
 		textField_1 = new JTextField();
 		textField_1.setText("Enter ID #");
 		textField_1.setColumns(10);
 		textField_1.setBounds(174, 10, 112, 19);
-		UpdateAnEquiptmentProblemPanel.add(textField_1);
+		UpdateAnEquipmentProblemPanel.add(textField_1);
 		
 		JLabel lblTypeOfUpdate = new JLabel("Type of update:");
 		lblTypeOfUpdate.setHorizontalAlignment(SwingConstants.LEADING);
 		lblTypeOfUpdate.setForeground(Color.BLACK);
 		lblTypeOfUpdate.setBounds(10, 39, 106, 13);
-		UpdateAnEquiptmentProblemPanel.add(lblTypeOfUpdate);
+		UpdateAnEquipmentProblemPanel.add(lblTypeOfUpdate);
 		
 		JComboBox comboBox_5_1 = new JComboBox();
 		comboBox_5_1.setModel(new DefaultComboBoxModel(new String[] {"Close/Resolve problem", "Description update"}));
 		comboBox_5_1.setBounds(126, 36, 160, 19);
-		UpdateAnEquiptmentProblemPanel.add(comboBox_5_1);
+		UpdateAnEquipmentProblemPanel.add(comboBox_5_1);
 		
 		JLabel lblUpdateDescription = new JLabel("Update description:");
 		lblUpdateDescription.setBounds(10, 62, 158, 13);
-		UpdateAnEquiptmentProblemPanel.add(lblUpdateDescription);
+		UpdateAnEquipmentProblemPanel.add(lblUpdateDescription);
 		
 		JTextArea txtrEnterDescription_1 = new JTextArea();
 		txtrEnterDescription_1.setText("Enter Description");
 		txtrEnterDescription_1.setBounds(110, 77, 321, 71);
-		UpdateAnEquiptmentProblemPanel.add(txtrEnterDescription_1);
+		UpdateAnEquipmentProblemPanel.add(txtrEnterDescription_1);
 		
 		JButton btnUpdateProblem = new JButton("Update Problem");
 		btnUpdateProblem.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnUpdateProblem.setBounds(138, 168, 166, 21);
-		UpdateAnEquiptmentProblemPanel.add(btnUpdateProblem);
+		UpdateAnEquipmentProblemPanel.add(btnUpdateProblem);
 		
-		JPanel QueryEquiptmentPanel = new JPanel();
-		tabbedPane.addTab("Query Equiptment Database", null, QueryEquiptmentPanel, null);
-		QueryEquiptmentPanel.setLayout(null);
+		JPanel QueryEquipmentPanel = new JPanel();
+		tabbedPane.addTab("Query Equipment Database", null, QueryEquipmentPanel, null);
+		QueryEquipmentPanel.setLayout(null);
 		
-		JLabel lblEquiptmentId_1 = new JLabel("Equiptment ID:");
-		lblEquiptmentId_1.setForeground(Color.DARK_GRAY);
-		lblEquiptmentId_1.setBounds(13, 31, 95, 13);
-		QueryEquiptmentPanel.add(lblEquiptmentId_1);
+		JLabel lblEquipmentId_1 = new JLabel("Equiptment ID:");
+		lblEquipmentId_1.setForeground(Color.DARK_GRAY);
+		lblEquipmentId_1.setBounds(13, 31, 95, 13);
+		QueryEquipmentPanel.add(lblEquipmentId_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
 		textField_2.setBounds(80, 45, 88, 19);
-		QueryEquiptmentPanel.add(textField_2);
+		QueryEquipmentPanel.add(textField_2);
 		
 		JLabel lblOr = new JLabel("OR");
 		lblOr.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOr.setFont(new Font("Tahoma", Font.BOLD, 12));
 		lblOr.setBounds(205, 47, 23, 17);
-		QueryEquiptmentPanel.add(lblOr);
+		QueryEquipmentPanel.add(lblOr);
 		
 		JLabel lblPatientId_1 = new JLabel("Equitpment type:");
 		lblPatientId_1.setForeground(Color.DARK_GRAY);
 		lblPatientId_1.setBounds(248, 31, 102, 13);
-		QueryEquiptmentPanel.add(lblPatientId_1);
+		QueryEquipmentPanel.add(lblPatientId_1);
 		
 		JComboBox comboBox_5_2 = new JComboBox();
 		comboBox_5_2.setModel(new DefaultComboBoxModel(new String[] {"Type #1", "Type #2", "..."}));
 		comboBox_5_2.setBounds(332, 45, 106, 19);
-		QueryEquiptmentPanel.add(comboBox_5_2);
+		QueryEquipmentPanel.add(comboBox_5_2);
 		
-		JLabel lblSearchForEquiptment = new JLabel("Search for equiptment by specifying an equiptment ID or an equiptment type.");
-		lblSearchForEquiptment.setBounds(10, 6, 452, 13);
-		QueryEquiptmentPanel.add(lblSearchForEquiptment);
+		JLabel lblSearchForEquipment = new JLabel("Search for equipment by specifying an equipment ID or an equipment type.");
+		lblSearchForEquipment.setBounds(10, 6, 452, 13);
+		QueryEquipmentPanel.add(lblSearchForEquipment);
 		
 		JButton btnQueryDatabase = new JButton("Query Database");
 		btnQueryDatabase.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnQueryDatabase.setBounds(156, 87, 138, 21);
-		QueryEquiptmentPanel.add(btnQueryDatabase);
+		QueryEquipmentPanel.add(btnQueryDatabase);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(2, 112, 460, 138);
-		QueryEquiptmentPanel.add(scrollPane);
+		QueryEquipmentPanel.add(scrollPane);
 		
 		JPanel VendorListMaintenancePanel = new JPanel();
 		tabbedPane.addTab("Vendor List Maintenance ", null, VendorListMaintenancePanel, null);
@@ -270,7 +270,7 @@ public class EQ {
 		JButton btnQueryVendorInformation = new JButton("Query Vendor Information");
 		btnQueryVendorInformation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frmEquiptmentInventory.setVisible(false);
+				frmEquipmentInventory.setVisible(false);
 				VendorListSubsystem.eqQueryVendorList();
 				
 			}
@@ -282,7 +282,7 @@ public class EQ {
 		JButton btnAddANew = new JButton("Add A New Vendor");
 		btnAddANew.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frmEquiptmentInventory.setVisible(false);
+				frmEquipmentInventory.setVisible(false);
 				VendorListSubsystem.eqNewVendor();
 			}
 		});
